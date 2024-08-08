cdfbe08e8715647739c0264d5774c1764f5d7685
Ivan Orozco <ivan.oroz34@gmail.com>
2024-08-08 10:39:56 -0500

    :zap: Updated to UDP communication

--- servidor_operacion1.py
+++ servidor_operacion1.py
@@ -6,26 +6,24 @@ def multiply_matrices(a, b):
     return np.dot(a, b).tolist()
 
 def main():
-    host = '192.168.56.3'  # Dirección IP del servidor de operación 1
-    port = 5001
+    host = '192.168.56.3'  # Cambia según el servidor
+    port = 5001  # Puerto correspondiente al servidor
 
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     server_socket.bind((host, port))
-    server_socket.listen(1)
 
-    print(f"Servidor de operación 1 escuchando en {host}:{port}/TCP")
+    print(f"Servidor de operacion escuchando en {host}:{port}")
 
     while True:
-        conn, addr = server_socket.accept()
+        data, addr = server_socket.recvfrom(4096)
         print(f"Conexión establecida desde {addr}")
 
-        data = conn.recv(4096).decode()
-        matrices = json.loads(data)
+        matrices = json.loads(data.decode())
 
         result = multiply_matrices(matrices['a'], matrices['b'])
-        conn.send(json.dumps(result).encode())
 
-        conn.close()
+        # Enviar el resultado de vuelta al servidor principal
+        server_socket.sendto(json.dumps(result).encode(), addr)
 
 if __name__ == "__main__":
     main()
